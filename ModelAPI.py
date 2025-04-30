44b3f8fa7629710abea08ea5dc44b8505debb44e
C0smic <vovantung0212@gmail.com>
2025-04-30 19:13:54 +0700

    feat: add API to connect to model

---
+++ ModelAPI.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from transformers import pipeline
+import uvicorn
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class TextRequest(BaseModel):
+    text: str
+
+def get_classifier():
+    try:
+        classifier = pipeline("text-classification", model="./emotion_model", tokenizer="./emotion_model", return_all_scores=True)
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
+    return classifier
+
+@app.post("/predict")
+async def predict(request: TextRequest):
+    classifier = get_classifier()
+    result = classifier(request.text)
+    return result
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
